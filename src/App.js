be50ccef7b12fd291464933305a77626042b1cfc
igor_999 <igoness@gmail.com>
2025-02-13 16:17:27 +0200

    Restore original App and index configurations with MobX and React Router

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
-// import React, { Component } from "react";
-// import { Routes, Route } from "react-router-dom";
-// import { inject, observer } from "mobx-react";
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
+import { inject, observer } from "mobx-react";
 
-// import SignInPage from "./pages/signin/SignInPage";
-// import SignUpPage from "./pages/signup/SignUpPage";
-// import TasksPage from "./pages/tasks/TasksPage";
-// import CreateTaskPage from "./pages/create-task/CreateTaskPage";
+import SignInPage from "./pages/signin/SignInPage";
+import SignUpPage from "./pages/signup/SignUpPage";
+import TasksPage from "./pages/tasks/TasksPage";
+import CreateTaskPage from "./pages/create-task/CreateTaskPage";
 
-// @inject("routerStore")
-// @observer
-// class App extends Component {
-//   render() {
-//     // Add this for debugging
-//     console.log("App component rendered");
-
-//     return (
-//       <Routes>
-//         <Route path="/" element={<SignInPage />} />
-//         <Route path="/signin" element={<SignInPage />} />
-//         <Route path="/signup" element={<SignUpPage />} />
-//         <Route path="/tasks" element={<TasksPage />} />
-//         <Route path="/tasks/create" element={<CreateTaskPage />} />
-//       </Routes>
-//     );
-//   }
-// }
-
-const App = () => {
-  console.log("Before return in App");
-
-  return (
-    <div>
-      <h1>Test Page</h1>
-    </div>
-  );
-};
-
-// Log that the file is being processed
-console.log("App.js is being processed");
+@inject("routerStore")
+@observer
+class App extends Component {
+  render() {
+    return (
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/tasks" element={<TasksPage />} />
+        <Route path="/tasks/create" element={<CreateTaskPage />} />
+      </Routes>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { Provider } from "mobx-react";
-// import { BrowserRouter } from "react-router-dom";
-// import { createBrowserHistory } from "history";
+import { Provider } from "mobx-react";
+import { BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-// import TasksService from "./services/tasks.service";
-// import TasksStore from "./stores/tasks.store";
-// import UserStore from "./stores/user.store";
-// import AuthService from "./services/auth.service";
-// import AppRouterStore from "./stores/router.store";
+import TasksService from "./services/tasks.service";
+import TasksStore from "./stores/tasks.store";
+import UserStore from "./stores/user.store";
+import AuthService from "./services/auth.service";
+import AppRouterStore from "./stores/router.store";
 
-// const services = {};
-// const stores = {};
+const services = {};
+const stores = {};
 
-// // Initialize services
-// services.tasksService = new TasksService();
-// services.authService = new AuthService();
+// Initialize services
+services.tasksService = new TasksService();
+services.authService = new AuthService();
 
-// // Initialize stores with services
-// stores.tasksStore = new TasksStore(services.tasksService);
-// stores.userStore = new UserStore(services.authService);
+// Initialize stores with services
+stores.tasksStore = new TasksStore(services.tasksService);
+stores.userStore = new UserStore(services.authService);
 
-// // Initialize router store
-// const browserHistory = createBrowserHistory();
-// stores.routerStore = new AppRouterStore();
-// stores.routerStore.setHistory(browserHistory);
+// Initialize router store
+const browserHistory = createBrowserHistory();
+stores.routerStore = new AppRouterStore();
+stores.routerStore.setHistory(browserHistory);
 
-// // Listen to history changes
-// browserHistory.listen((update) => {
-//   stores.routerStore.setLocation(update.location);
-// });
+// Listen to history changes
+browserHistory.listen((update) => {
+  stores.routerStore.setLocation(update.location);
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <Provider {...stores}>
-//     <BrowserRouter basename={process.env.PUBLIC_URL} history={browserHistory}>
-//       <App />
-//     </BrowserRouter>
-//   </Provider>
-// );
-root.render(<App />);
+root.render(
+  <Provider {...stores}>
+    <BrowserRouter basename={process.env.PUBLIC_URL} history={browserHistory}>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
